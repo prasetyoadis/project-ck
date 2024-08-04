3740945a841aaeaf04a88e13e3964adb3f3039ad
lidyanurmala <evanurmalalidya21@gmail.com>
2024-08-04 18:02:30 +0700

    Membuat Halaman Landing Page

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="bg-secondary">
+    <p class="text-center">&copy; 2024 CeritaKita. All rights reserved.</p>
+
+    <p class="m-2 justify">Visit Me: <br>
+        Jl. Siliwangi, Jombor Lor, Sendangadi, <br>
+        Kec. Mlati, Kabupaten Sleman, <br>
+        Daerah Istimewa Yogyakarta 55285
+    </p>
+</footer>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,32 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home',[
+        "title" => "Home"
+    ]);
+});
+Route::get('/about', function () {
+    return view('about', [
+        "title" => "About"
+    ]);
+});
+Route::get('/help', function () {
+    return view('help',[
+        "title" => "Help"    
+    ]);
+});
+Route::get('/katalog', function () {
+    return view('katalog', [
+        "title" => "Katalog"
+    ]);
+});
+Route::get('/blog', function () {
+    return view('Blog', [
+        "title" => "Blog"
+    ]);
+});
+Route::get('/testimoni', function () {
+    return view('testimoni', [
+        "title" => "Testimoni"
+    ]);
 });
