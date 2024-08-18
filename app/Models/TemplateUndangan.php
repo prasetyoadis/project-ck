da7a2a0379559a6af45a969711fb1841c41fbe5c
tunetun123 <fathurrahmansyuaib@gmail.com>
2024-08-18 11:03:37 +0800

    menyelesaikan model TemplateUndangan, revisi model order dan testimoni

--- app/Models/TemplateUndangan.php
+++ app/Models/TemplateUndangan.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class TemplateUndangan extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'nama_template',
+        'category',
+        'is_active',
+    ];
+
+    public function order() {
+        return $this->belongsTo(Order::class);
+    }
 }
--- app/Models/Testimoni.php
+++ app/Models/Testimoni.php
@@ -4,15 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Testimoni extends Model
 {
     use HasFactory;
 
     protected $fillable = [
-        'name',
-        'email',
         'testimoni',
         'rating',
     ];
+
+    public function order() {
+        return $this->hasOne(Order::class);
+    }
 }
