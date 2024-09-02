bd9609033e9f96392877393b4f25855156fbb820
prasetyoadis <prasetyoadi522@gmail.com>
2024-09-02 18:12:38 +0700

    Menyelesaikan login dashboard dan testing login

---
+++ app/Http/Controllers/ThameController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Thame;
+use App\Http\Requests\StoreThameRequest;
+use App\Http\Requests\UpdateThameRequest;
+
+class ThameController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreThameRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Thame $thame)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Thame $thame)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateThameRequest $request, Thame $thame)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Thame $thame)
+    {
+        //
+    }
+}
